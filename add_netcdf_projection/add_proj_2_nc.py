55a6b3a9aac055ee5ce28a82cd393ad37b55a709
Micah Johnson <micah.johnson150@gmail.com>
2019-01-09 10:44:53 -0700

    Made the projection script more robust

--- add_netcdf_projection/add_proj_2_nc.py
+++ add_netcdf_projection/add_proj_2_nc.py
@@ -4,10 +4,14 @@ import os.path
 from urllib.request import urlopen
 import re
 
-def strip_chars(edit_str, bad_chars='[(){}<>,"]=\n'):
-    return re.sub(bad_chars, '', edit_str)
 
-def gather_utm_meta(epsg_str):
+def strip_chars(edit_str, bad_chars='[(){}<>,"_]=\nns'):
+    result = ''.join([s for s in edit_str if s not in bad_chars])
+    print(result)
+    return result
+
+
+def gather_utm_meta(epsg_str,epsg):
     """
     Use if the EPSG data is associated to UTM
     Gathers the data and returns a dictionary of data and attributes that need
@@ -16,24 +20,26 @@ def gather_utm_meta(epsg_str):
 
     """
     meta = epsg_str.lower()
+
     map_meta = {
                     "grid_mapping_name": "universal_transverse_mercator",
                     "utm_zone_number": None,
                     "semi_major_axis": None,
                     "inverse_flattening": None,
-                    "_CoordinateTransformType": "Projection",
+                    'spatial_ref':epsg_str,
+                    "_CoordinateTransformType": "projection",
                     "_CoordinateAxisTypes": "GeoX GeoY"}
 
     # Assign the zone number
     zone_str = meta.split('zone')[1]
-    map_meta['utm_zone_number'] = int(strip_chars(zone_str.split(',')[0])[0:-2])
+    map_meta['utm_zone_number'] = float((strip_chars(zone_str.split(',')[0])).strip()[-2:])
 
     # Assign the semi_major_axis
     axis_string = meta.split('spheroid')[1]
     map_meta['semi_major_axis'] = float(axis_string.split(',')[1])
 
     # Assing the flattening
-    map_meta["inverse_flattening"] = float(axis_string.split(',')[2])
+    map_meta["inverse_flattening"] = float(strip_chars(axis_string.split(',')[2]))
 
     return map_meta
 
@@ -74,6 +80,7 @@ def copy_nc(infile, outfile, exclude=None):
                 dst[name].setncatts(src[name].__dict__)
     return dst
 
+
 def add_proj(nc_obj,epsg):
     """
         Adds the appropriate attributes to the netcdf for managing projection info
@@ -86,34 +93,51 @@ def add_proj(nc_obj,epsg):
     """
     # function to generate .prj file information using spatialreference.org
     # access projection information
-    wkt = urlopen("http://spatialreference.org/ref/epsg/{0}/prettywkt/".format(epsg))
+    try:
+        wkt = urlopen("http://spatialreference.org/ref/epsg/{0}/prettywkt/".format(epsg))
+    except:
+        wkt = urlopen("http://spatialreference.org/ref/sr-org/{0}/prettywkt/".format(epsg))
 
     # remove spaces between charachters
     remove_spaces = ((wkt.read()).decode('utf-8')).replace(" ","")
+    # Add in the variable for holding coordinate system info
+    map_meta = parse_wkt(remove_spaces,epsg)
 
-    map_meta = parse_wkt(remove_spaces)
+    nc_obj.createVariable("projection","S1")
+    nc_obj["projection"].setncatts(map_meta)
+
     for name,var in nc_obj.variables.items():
-        print(var.dimensions)
-        gridded = [d for d in var.dimensions if 'x' in d.lower() or 'y' in d.lower()]
 
         # Assume all 2D+ vars are the same projection
-        if len(gridded)>1:
-            print("gridded")
-            nc_obj[name].setncatts(map_meta)
+        if 'x' in var.dimensions and 'y' in var.dimensions:
+            print("Adding Coordinate System info to {0}".format(name))
+            nc_obj[name].setncatts({"grid_mapping":"projection"})
+
+        elif name.lower() in ['x','y']:
+            # Set a standard name, which is required for recognizing projections
+            nc_obj[name].setncatts({"standard_name":"projection_{}_coordinate"
+            "".format(name.lower())})
+            # Set the units
+            nc_obj[name].setncatts({"units":"meters"
+            "".format(name.lower())})
 
 
     return nc_obj
 
-def parse_wkt(epsg_string):
+
+def parse_wkt(epsg_string,epsg):
     """
     """
     map_meta = {}
     wkt_data = (epsg_string.lower()).split(',')
 
     if 'utm' in epsg_string.lower():
-        map_meta = gather_utm_meta(epsg_string)
-        print(map_meta)
+        map_meta = gather_utm_meta(epsg_string,epsg)
+    print(" Meta information to be added...\n")
+    for k,v in map_meta.items():
+        print(k,repr(v))
     return map_meta
+
 
 def main():
     p = argparse.ArgumentParser(description= "Add projection information to a"
