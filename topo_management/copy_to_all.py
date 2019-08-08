78834fbdac74cee3c26904b5e298befc7efb84d7
Micah Johnson <micah.johnson150@gmail.com>
2019-08-08 12:33:56 -0600

    Working on running all the topos. Also added in a file copying function:

---
+++ topo_management/copy_to_all.py
@@ -0,0 +1,17 @@
+from make_all_topos import find_basin_paths
+import os
+
+basins_dir = "~/projects/basins"
+copy_file = "~/projects/basins/docker-compose.yml"
+
+
+copy_file = os.path.expanduser(copy_file)
+basins_dir = os.path.expanduser(basins_dir)
+
+paths = find_basin_paths(basins_dir, indicator_folder="model_setup",
+                                     indicator_file="Makefile")
+print("Copying file to {} locations...".format(len(paths)))
+
+for r in paths:
+    print("Copying {} to {}".format(os.path.basename(copy_file), r))
+    os.system("cp {} {}".format(copy_file, r))
