1caf0a978296166cf7caa6a077fc58075f1099bb
Micah Johnson <micah.johnson150@gmail.com>
2020-01-24 14:17:04 -0700

    Added more functionality for copying over files and jazz

--- topo_management/copy_one_to_all.py
+++ topo_management/copy_one_to_all.py
@@ -1,22 +1,35 @@
-from make_all_topos import find_basin_paths
-import os
+from make_topos import find_basin_paths
+from os.path import expanduser, basename
+from os import system
+import argparse
+
 
 """
-Copies a file to all the basin folders, Originally designed for docker-compose
-files
+Originally written to copy a file to all the basin folders e.g. think
+docker-compose.yml
 """
 
-basins_dir = "~/projects/basins"
-copy_file = "~/projects/basins/tuolumne/model_setup/docker-compose.yml"
+parser = argparse.ArgumentParser(
+         description='Copies one file to every folder under the basins folder'
+                     ' containing the indicator_folder and indicator_file ')
+parser.add_argument('file', help='Filename to copy')
+parser.add_argument('--basin','-b', dest='basins',default='~/projects/basins',
+                    help='Directory to copy a file to every sub directory to.')
+parser.add_argument('--ind_file','-if', dest='ind_f', default=None,
+                    help='If this file name is in a sub director then its a '
+                         'good candidate to copy the file to')
+parser.add_argument('--ind_dir','-id', dest='ind_d', default=None,
+                    help='If this director name is in a sub directory then its '
+                         'a good candidate to copy the file to')
+args = parser.parse_args()
 
+basins_dir = expanduser(args.basins)
+copy_file = expanduser(args.file)
 
-copy_file = os.path.expanduser(copy_file)
-basins_dir = os.path.expanduser(basins_dir)
-
-paths = find_basin_paths(basins_dir, indicator_folder="model_setup",
-                                     indicator_file="Makefile")
+paths = find_basin_paths(basins_dir, indicator_folder=args.ind_d,
+                                     indicator_file=args.ind_f)
 print("Copying file to {} locations...".format(len(paths)))
 
 for r in paths:
-    print("Copying {} to {}".format(os.path.basename(copy_file), r))
-    os.system("cp {} {}".format(copy_file, r))
+    print("Copying {} to {}".format(basename(copy_file), r))
+    system("cp {} {}".format(copy_file, r))
