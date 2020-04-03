85e2ce46cfd220f818488946797abd27d7423058
Micah Johnson <micah.johnson150@gmail.com>
2020-04-03 14:10:04 -0600

    QGIS nughtmare scripts

---
+++ install_packages/install_qgis2_ubuntu_16-04.sh
@@ -0,0 +1,5 @@
+#sudo sh -c 'echo "deb http://qgis.org/debian xenial main" >> /etc/apt/sources.list'
+#sudo sh -c 'echo "deb-src http://qgis.org/debian xenial main" >> /etc/apt/sources.list'
+
+sudo apt-get update
+sudo apt-get install qgis python-qgis qgis-plugin-grass:
